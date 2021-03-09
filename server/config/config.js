ae23d0b9f721b03a40d323900ab47a3ba4e7735f
Jorge Mejía <jorgeandresmejiasuchite@gmail.com>
2021-03-09 17:28:05 -0600

    V1

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+//Process es un objeto que corre siempre y se actualiza dependiendo del environment
+// -----------------------------------------
+// ----------------Puerto-------------------
+// -----------------------------------------
+process.env.PORT = process.env.PORT || 3000;
